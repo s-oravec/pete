46480924633b7fd3e8a95ea1e9696727d9daab5c
Stefan Oravec <stefan.oravec@me.com>
2015-05-03 21:51:49 +0200

    pete_assert tests

--- test/package/run.sql
+++ test/package/run.sql
@@ -20,5 +20,14 @@ prompt Run test package UT_PETE
 prompt ===============================
 exec pete.run(a_package_name_in => 'UT_PETE_CONFIG_RUNNER');
 prompt
+prompt
+prompt Install test package UT_PETE_ASSERT
+prompt ==================================
+@@ut_pete_assert.pck
+prompt
+prompt Run test package UT_PETE_ASSERT
+prompt ===============================
+exec pete.run(a_package_name_in => 'UT_PETE_ASSERT');
+prompt
 
 @&&run_dir_end
