5456cf1c28e7635268ad13453aee301914e0444e
Michal Pravda <michal.pravda@gmail.com>
2015-05-07 23:31:51 +0200

    param->argument

--- application/schema/package/pete.spec.sql
+++ application/schema/package/pete.spec.sql
@@ -27,7 +27,7 @@ CREATE OR REPLACE PACKAGE pete AS
             if \e pete_after_each.run
         if \e pete_after_all.run
     
-    run for other optional params will run "subset": a_package_in - jednu package
+    run for other optional arguments will run "subset": a_package_in - jednu package
     a_script in - jeden skript
     a_method_in - jednu metodu
     a_case_in - jeden test case.
@@ -39,19 +39,19 @@ CREATE OR REPLACE PACKAGE pete AS
     -- Runs test
     -- Universal run procedure. Can be used to run any unit of work of either testing style. 
     -- Other public run procedures call this one. It accepts only 
-    -- - configuration parameters (a_suite_name_in, a_script_name_in, a_case_name_in) or
-    -- - conventional parameters (a_suite_name_in, a_package_name_in, a_method_name_in) 
+    -- - configuration arguments (a_suite_name_in, a_script_name_in, a_case_name_in) or
+    -- - conventional arguments (a_suite_name_in, a_package_name_in, a_method_name_in) 
     -- not a combination from both sets
-    -- testing style can be explicitly set by a_style_conventional_in parameter
+    -- testing style can be explicitly set by a_style_conventional_in argument
     --
-    -- %param a_suite_name_in Runs a suite of tests of a given name. If there are suites of both testing styles
+    -- %argument a_suite_name_in Runs a suite of tests of a given name. If there are suites of both testing styles
     -- then throws an ge_ambiguous_input exception 
     --
-    -- %param a_package_name_in Runs all tests following convention in a given package 
-    -- %param a_method_name_mask_in Runs only tests of a given mask in a given package. Must be used with param a_package_in
-    -- %param a_script_name_in Runs a test script of a given name
-    -- %param a_case_name_in Runs a test case of a given name
-    -- %param a_style_conventional If true
+    -- %argument a_package_name_in Runs all tests following convention in a given package 
+    -- %argument a_method_name_mask_in Runs only tests of a given mask in a given package. Must be used with argument a_package_in
+    -- %argument a_script_name_in Runs a test script of a given name
+    -- %argument a_case_name_in Runs a test case of a given name
+    -- %argument a_style_conventional If true
     --
     -- %throws ge_ambiguous_input 
     -- %throws ge_conflicting_input 
@@ -70,7 +70,7 @@ CREATE OR REPLACE PACKAGE pete AS
     ge_ambiguous_input EXCEPTION;
     gc_AMBIGUOUS_INPUT CONSTANT PLS_INTEGER := -20001;
     PRAGMA EXCEPTION_INIT(ge_ambiguous_input, -20001);
-    -- Thrown if more conflicting parameters are set
+    -- Thrown if more conflicting arguments are set
     ge_conflicting_input EXCEPTION;
     gc_CONFLICTING_INPUT CONSTANT PLS_INTEGER := -20001;
     PRAGMA EXCEPTION_INIT(ge_ambiguous_input, -20002);
@@ -78,8 +78,8 @@ CREATE OR REPLACE PACKAGE pete AS
     --
     -- Runs a suite
     --
-    -- %param a_suite_name_in 
-    -- %param a_style_conventional
+    -- %argument a_suite_name_in 
+    -- %argument a_style_conventional
     --
     -- %throws ge_ambiguous_input If the input can't be clearly interpreted
     --
@@ -92,14 +92,14 @@ CREATE OR REPLACE PACKAGE pete AS
     --
     -- Runs a script identified by name
     --
-    -- %param a_script_name_in name of the script to be run
+    -- %argument a_script_name_in name of the script to be run
     --
     PROCEDURE run_test_script(a_script_name_in IN VARCHAR2);
 
     --
     -- Runs a script identified by name
     --
-    -- %param a_script_name_in name of the script to be run
+    -- %argument a_script_name_in name of the script to be run
     --
     PROCEDURE run_test_case(a_case_name_in IN VARCHAR2);
 
@@ -107,10 +107,10 @@ CREATE OR REPLACE PACKAGE pete AS
     -- Runs tests for a given package. Such tests are in a test package which can be derived from the given one.    
     -- throws tests not found if there are no tests to be run
     --
-    -- %param a_package_in 
-    -- %param a_method_name_like_in 
-    -- %param a_is_test_package_in 
-    -- %param a_prefix_in 
+    -- %argument a_package_in 
+    -- %argument a_method_name_like_in 
+    -- %argument a_is_test_package_in 
+    -- %argument a_prefix_in 
     --
     PROCEDURE run_test_package
     (
