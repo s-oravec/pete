30b456db9c140ee3af18b077e289a574d3fc82d5
Štefan Oravec <stefan.oravec@me.com>
2015-09-24 17:26:25 +0200

    timing information

--- application/schema/view/petev_output_run_log.sql
+++ application/schema/view/petev_output_run_log.sql
@@ -1,9 +1,20 @@
-create or replace force view petev_output_run_log
- as
+CREATE OR REPLACE FORCE VIEW petev_output_run_log
+ AS
 SELECT lpad(' ', (LEVEL - 1) * 2, ' ') || --x
-       case when object_type = 'ASSERT' then '  ' end || --x
+       CASE
+           WHEN object_type = 'ASSERT' THEN
+            '  '
+       END || --x
        description || --x
-       ' - ' || decode(result, 0, 'SUCCESS', 'FAILURE') || --x
+       ' - ' || decode(RESULT, 0, 'SUCCESS', 'FAILURE') || --x
+       ' in ' || CASE
+           WHEN test_end - test_begin < NUMTODSINTERVAL(1, 'minute') THEN
+            '0' || to_char(EXTRACT(SECOND FROM test_end - test_begin)) || 's'
+           WHEN test_end - test_begin >= NUMTODSINTERVAL(1, 'day') THEN
+            regexp_replace(to_char(test_end - test_begin), '\+0+', '') || 'd'
+           ELSE
+            regexp_replace(to_char(test_end - test_begin), '\+0+ ', '') || 'h'
+       END || --
        CASE
            WHEN l.error_stack IS NOT NULL THEN
             chr(10) || chr(10) || error_stack || chr(10) || error_backtrace ||
@@ -11,9 +22,9 @@ SELECT lpad(' ', (LEVEL - 1) * 2, ' ') || --x
        END log,
        object_type,
        RESULT,
-       decode(result, 0, 'SUCCESS', 'FAILURE') result_str
+       decode(RESULT, 0, 'SUCCESS', 'FAILURE') result_str
   FROM pete_run_log l
  START WITH id = pete_logger.get_output_run_log_id
 CONNECT BY PRIOR id = parent_id
- ORDER BY id
+ ORDER BY ID
 ;
