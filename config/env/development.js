dcfc759284408a4f5963c5cd905c4cb744a34247
Stefan Oravec <stefan.oravec@me.com>
2018-08-22 10:30:06 +0200

    - ditch grunt

--- config/env/development.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-
-module.exports = {
-    db: {
-        superUserDbConnectString:    'sys/oracle@local as sysdba',
-        appUserDbConnectString:     'PETE_000200_DEV/PETE_000200_DEV@local',
-        testedSchemaDbConnectString: 'PETE_000200_OTH/PETE_000200_OTH@local'
-    },
-};
--- config/env/test.js
+++
@@ -1,8 +0,0 @@
-'use strict';
-
-module.exports = {
-    db: {
-        superUserDbConnectString: process.env.PETE_SUPERUSER_CONN || 'PETE_SUPERUSER_CONN',
-        appUserDbConnectString:  process.env.PETE_PETEUSER_CONN || 'PETE_PETEUSER_CONN'
-    },
-};
